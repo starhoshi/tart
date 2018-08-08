b0b5783dd5b2b51ee94ecd8dc4e59d0ed8aa2185
Kensuke Hoshikawa <kensuke-hoshikawa@cookpad.com>
2018-08-08 21:37:37 +0900

    update index.d.ts

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,7 @@
 import * as FirebaseFirestore from '@google-cloud/firestore';
+declare type Partial<T> = {
+    [P in keyof T]?: T[P];
+};
 export declare const initialize: (_firestore: FirebaseFirestore.Firestore) => void;
 export declare class Snapshot<T extends Timestamps> {
     ref: FirebaseFirestore.DocumentReference;
@@ -15,12 +18,8 @@ export declare class Snapshot<T extends Timestamps> {
     saveNestedCollection<S extends Timestamps>(collectionName: string, snapshot: Snapshot<S>): Promise<FirebaseFirestore.WriteResult>;
     saveNestedCollectionWithBatch<S extends Timestamps>(batch: FirebaseFirestore.WriteBatch, collectionName: string, snapshot: Snapshot<S>): void;
     fetchNestedCollections<S extends Timestamps>(collectionName: string): Promise<Snapshot<S>[]>;
-    update(data: {
-        [id: string]: any;
-    }): Promise<FirebaseFirestore.WriteResult>;
-    updateWithBatch(batch: FirebaseFirestore.WriteBatch, data: {
-        [id: string]: any;
-    }): void;
+    update(data: Partial<T>): Promise<FirebaseFirestore.WriteResult>;
+    updateWithBatch(batch: FirebaseFirestore.WriteBatch, data: Partial<T>): void;
     delete(): Promise<FirebaseFirestore.WriteResult>;
     deleteWithBatch(batch: FirebaseFirestore.WriteBatch): void;
 }
@@ -30,3 +29,4 @@ export interface Timestamps {
 }
 export declare const makeNotSavedSnapshot: <T extends Timestamps>(path: string, data: T, id?: string | undefined) => Snapshot<T>;
 export declare const fetch: <T extends Timestamps>(pathOrDocumentReference: string | FirebaseFirestore.DocumentReference, id?: string | undefined) => Promise<Snapshot<T>>;
+export {};
