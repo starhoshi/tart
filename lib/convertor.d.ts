c0a2af84bbeb25e3286295a33f91fc973d4c1bb8
tera-ny <haru25haru02@gmail.com>
2021-01-21 21:51:30 +0900

    add build files

---
+++ lib/convertor.d.ts
@@ -0,0 +1,3 @@
+import { Timestamps } from './';
+export declare const convertToInput: <T extends Timestamps>(data: T) => any;
+export declare const convertToOutput: <T extends Timestamps>(data: T) => any;
